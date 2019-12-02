d9fba8dc785b57cd97b8d2ca25228931e0cd0454
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-02 10:48:38 -0500

    import TodoList to App.js for console logging object

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,3 +2,12 @@
 //Your todo list should display a list of todos, an input field, a submit button, and a clear all button.
 
 import React, { Component } from 'react'
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
+}
+
+export default TodoList;
